7e3188ed3c81ee8b7209c8ecee54a492771600dc
Rana <ranab33@gmail.com>
2021-01-04 19:13:06 -0500

    initial commit

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+# + addition
+# - subtraction
+# * multiplication
+# / division
+# ** exponent
+# % modulo
+# // integer division
+
+# Variables and Datatypes
+# a variable in python works just like in math where the variable needs to be assigned first 
+
+num_of_cats = 99
+
+num_of_cats
+
+#Variable Assignment
+# variables can be: assigned to other variables, reassigned at any time.

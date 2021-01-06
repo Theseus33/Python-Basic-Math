c7c0c043e3c7150c4862b6b7e975977d4ad8e9fc
Rana <ranab33@gmail.com>
2021-01-05 22:47:48 -0500

    basic concepts

--- basic_math.py
+++ basic_math.py
@@ -15,3 +15,69 @@ num_of_cats
 
 #Variable Assignment
 # variables can be: assigned to other variables, reassigned at any time.
+
+#Variable Restrictions
+
+1. Must start with a letter or underscore.
+2. The  rest of the name must consist of letters, numbers, or underscores.
+3. Names are case sensitive.
+
+#Standard Style Conventions
+
+1. Most variables shoudl be snake_case (underscore between words)
+2. Most vairables should also be lowercase, with some exceptions
+    ie: CAPITAL_SNAKE_CASE usually referes to contansts (eg Pi = 3.14)
+        UpperCamelCase usually refers to a class
+3. Variables that start and end with two underscores (called "dunder" ) are supposed to be private
+or left alone
+    ie: __dont_touch__
+
+#Data Types Overview
+
+1. Booleans (True/False)
+2. Intergers (1,2,3)
+3. Strings (a sequence of unicode or characters)
+4. Lists (an ordered sequence of values of other data types)
+5. Dictionary (a collection of key:value pairs)
+
+#Dynamic Typing
+Python is highly flexible about reassigning variables to different types. Varibales can change 
+values as well as types.
+
+awesomeness = True (Boolean)
+awesomeness = "a dog" (String)
+awesomeness = None (special value)
+Awesomeness = 22 / 7 (float)
+
+# The Special Value None
+needs a capital "N" - None
+represents nothingness in python
+similar to Null in other languages
+
+#Declaring Stings
+String literals in Python can be decalared wit heither single or double quotes but be consistant!
+
+#String Escape Characters/Sequences
+In Python there are also "escape characters", which are "metacharacters" - they get interpreted by
+Python to do something special:
+
+All escape characers start with a backslash "\"
+new_line = "hello \n world"
+print(new_line)
+
+returns the following 
+#hello
+#world
+
+so if you actually want a backslash you need \\ or else Python will think its the start of an escape char
+
+  
+
+
+so if you actually want a backslash you need \\ or else Python will think its the start of an escape char
+
+
+
+
+
+

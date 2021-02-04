2ac3c46f77563d4cf339096b813ab3f83ec401aa
Rana <ranab33@gmail.com>
2021-02-03 20:00:41 -0500

    bouncer code along

--- basic_math.py
+++ basic_math.py
@@ -222,6 +222,11 @@ age = input("How old are you:")
 if int(age) >= 18 and int(age) < 21:
     print("You can enter, but need a wristband!")
 # 18-21 wristband
+elif age >= 21:
 # 21+ drink, normal entry
+    print("You are good to enter and can drink!")
+else:
 # too young sorry
-
+    print("You cannot come in, little one! :(")
+else:
+    print("Please enter an age!")
